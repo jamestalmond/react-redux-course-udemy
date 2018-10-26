096c910612ce36010a8e960e05a49ff24ad8da32
James <jamestalmond@gmail.com>
2018-10-26 15:23:35 +0100

    8.116

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,12 @@
 import React, { Fragment } from 'react';
 
+import classes from './Layout.css';
+
 const layout = (props) => (
 	<Fragment>
-		<div>
-			Toolbabr, SideDrawer, Backdrop
-		</div>
-		<main>
+		<div>Toolbar, SideDrawer, Backdrop</div>
+
+		<main className={classes.content}>
 			{props.children}
 		</main>
 	</Fragment>
